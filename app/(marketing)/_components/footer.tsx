2cf4283444acc06b147ba4c205286d5e0e43236b
mark-torres10 <markptorres1@gmail.com>
2023-10-19 21:45:57 +0800

    feat: first pass of landing page

---
+++ app/(marketing)/_components/footer.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo from "./logo";
+import { Button } from "@/components/ui/button";
+
+function Footer() {
+  return (
+    <div className="flex items-center w-full p-6 bg-background z-50">
+      <Logo />
+      <div
+        className="md:ml-auto w-full justify-between md:justify-end flex
+      items-center gap-x-2 text-muted-foreground"
+      >
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms of Service
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
